3094d06b6b1a6881063ff63beaa479f1bf906bca
Will Lee <will@willlee.ca>
2023-04-20 09:03:47 -0600

    Use br for divider and adjust media query

--- Components/Divider.jsx
+++ Components/Divider.jsx
@@ -1,9 +1,7 @@
-import React from "react"
+import React from "react";
 
 const Divider = () => {
-    return (
-        <div className="divider"></div>
-    )
-}
+  return <hr className="divider"></hr>;
+};
 
-export default Divider
+export default Divider;
